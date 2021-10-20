e4d8a04d383fec507687aed20799e41f28e5fabe
enoxy <enoxy@DESKTOP-8BHQKRR>
2021-10-20 16:24:00 +0200

    Добавьте файлы проекта.

---
+++ 4var 2.2 visoki uroven/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _4var_2._2_visoki_uroven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Введите число копеек");
+                double x = double.Parse(Console.ReadLine());
+                if (x >= 11 && x <= 14) Console.WriteLine("{0} копеек", x);
+                else
+                {
+                    double y = x % 10;
+                    switch (y)
+                    {
+                        case 0: Console.WriteLine("{0} копеек", x); break;
+                        case 1: Console.WriteLine("{0} копейка", x); break;
+                        case 2: case 3: case 4: Console.WriteLine("{0} копейки", x); break;
+                        default: Console.WriteLine("{0} копеек", x); break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
